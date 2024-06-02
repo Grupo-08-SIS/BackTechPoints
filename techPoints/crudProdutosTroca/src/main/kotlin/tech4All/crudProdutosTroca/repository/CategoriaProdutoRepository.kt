79ca10fd235a340ca28f8ea520880e7e46425dec
RafaelH31 <125298353+RafaelH31@users.noreply.github.com>
2024-06-02 20:06:56 -0300

    Delete techPoints/crudProdutosTroca directory

--- techPoints/crudProdutosTroca/src/main/kotlin/tech4All/crudProdutosTroca/repository/CategoriaProdutoRepository.kt
+++
@@ -1,8 +0,0 @@
-package tech4All.crudProdutosTroca.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import tech4All.crudProdutosTroca.domain.CategoriaProduto
-
-interface CategoriaProdutoRepository : JpaRepository<CategoriaProduto, Int> {
-
-}
--- techPoints/crudProdutosTroca/src/main/kotlin/tech4All/crudProdutosTroca/repository/ProdutoRepository.kt
+++
@@ -1,7 +0,0 @@
-package tech4All.crudProdutosTroca.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import tech4All.crudProdutosTroca.domain.Produto
-
-interface ProdutoRepository : JpaRepository<Produto, Int> {
-}
