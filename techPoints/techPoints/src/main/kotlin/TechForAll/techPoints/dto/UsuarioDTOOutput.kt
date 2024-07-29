6811559a4503860e7bb3a6ca7c7b87b1b82b28d9
brudneyuk <brudney.ramosjr@sptech.school>
2024-07-29 10:36:34 -0300

    Update: Tirando o @JsonIgnore de deletado, para poder criar a validação do login

--- techPoints/techPoints/src/main/kotlin/TechForAll/techPoints/dto/UsuarioDTOOutput.kt
+++ techPoints/techPoints/src/main/kotlin/TechForAll/techPoints/dto/UsuarioDTOOutput.kt
@@ -6,22 +6,21 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 import java.time.LocalDateTime
 
 data class UsuarioDTOOutput(
-    val idUsuario: Int? = null,
-    val nomeUsuario: String? = null,
-    val cpf: String? = null,
-    @JsonIgnore
-    val senha: String? = null,
-    val primeiroNome: String? = null,
-    val sobrenome: String? = null,
-    val email: String? = null ,
-    @JsonIgnore
-    val autenticado: Boolean? = null,
-    val dataCriacao: LocalDateTime? = null,
-    @JsonIgnore
-    val deletado: Boolean? = null,
-    @JsonIgnore
-    val dataDeletado: LocalDateTime? = null,
-    val dataAtualizacao: LocalDateTime? = null,
-    val endereco: Endereco? = null,
-        val tipoUsuario: Int? = null
+        val idUsuario: Int? = null,
+        val nomeUsuario: String? = null,
+        val cpf: String? = null,
+        @JsonIgnore
+        val senha: String? = null,
+        val primeiroNome: String? = null,
+        val sobrenome: String? = null,
+        val email: String? = null,
+        @JsonIgnore
+        val autenticado: Boolean? = null,
+        val dataCriacao: LocalDateTime? = null,
+        val deletado: Boolean? = null,
+        @JsonIgnore
+        val dataDeletado: LocalDateTime? = null,
+        val dataAtualizacao: LocalDateTime? = null,
+        val endereco: Endereco? = null,
+        val tipoUsuario: Int? = null,
 )
